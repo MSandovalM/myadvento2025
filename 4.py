285dabc02acb57d82069d1c1ca527ffd45b173ff
Manuel Sandoval Mendoza <msandovalm01@gmail.com>
2025-12-15 12:38:15 -0600

    add: fourth day second part

--- 4.py
+++ 4.py
@@ -14,20 +14,66 @@ TEST_INPUT = """..@@.@@@@.
 @.@.@@@.@.
 """
 
-TEST_OUTPUT = 13
+TEST_OUTPUT_FIRST = 13
+TEST_OUTPUT_SECOND = 43
 
+def second_part(raw_data: str): 
+
+    raw_base_map = raw_data
+
+    coor_rolls = first_part(raw_base_map)
+
+    raw_base_map = uptade_roll_map(coor_rolls, raw_base_map)
+
+    moved_rolls = len(coor_rolls)
+
+    while len(coor_rolls) > 0:
+        raw_base_map = uptade_roll_map(coor_rolls, raw_base_map)
+        coor_rolls.clear()
+        coor_rolls = first_part(raw_base_map)
+        moved_rolls += len(coor_rolls)
+
+    return moved_rolls
+    
 def first_part(raw_entry: str):
     base_map = raw_entry.splitlines()
     roll_char = '@'
     moved_rolls = 0
+
+    pos_rolls = []
+
     for row, line in enumerate(base_map):
         for column, char in enumerate(line):
             if char == roll_char:
                 if check_for_rolls(column, row, base_map):
+                    pos_rolls.append((row, column))
                     moved_rolls += 1
 
-    return moved_rolls
+    return pos_rolls
 
+"""
+Para la segunda parte vamos a guardar la row, col de cada rollo movido, al finalizar el mapa
+Pasamos esta lista de coordenadas para cambiar '@' por '.' y volviendo a pasar por el proceso
+mientras que moved_rolls sea distinto de cero vamos a seguir haciendo esto y registrando cuantos
+rollos se movieron cada vez que se hizo el proceso 
+"""
+
+def uptade_roll_map(moved_coor:list[tuple], base_map: str)-> str:
+    splited_map = base_map.splitlines()
+    new_map = ""
+    
+    for row, line in enumerate(splited_map):
+        new_line = ""
+        for col, char in enumerate(line):
+            
+            if (row, col) in moved_coor:
+                new_line += "."
+            else:
+                new_line += char
+        new_map += new_line + "\n"
+
+    return new_map
+                    
 
 def check_for_rolls(posx: int, posy: int, base_map: list[list[str]]) -> bool:
     
@@ -194,16 +240,29 @@ if __name__ == "__main__":
 
     test = False
 
-    if test:
-        res_val = first_part(TEST_INPUT)
+    # if test:
+    #     res_val = first_part(TEST_INPUT)
 
-        if res_val == TEST_OUTPUT:
+    #     if res_val == TEST_OUTPUT:
+    #         print("Passed: ", res_val)
+    #     else: 
+    #         print("Not Passed: ", res_val)
+    # else:
+    #     try:
+    #         res_val = first_part(personal_input)
+    #         print(res_val)
+    #     except ValueError:
+    #         print("Something went wrong :(")
+
+    if test: 
+        res_val = second_part(TEST_INPUT)
+        if res_val == TEST_OUTPUT_SECOND:
             print("Passed: ", res_val)
         else: 
             print("Not Passed: ", res_val)
     else:
         try:
-            res_val = first_part(personal_input)
+            res_val = second_part(personal_input)
             print(res_val)
         except ValueError:
             print("Something went wrong :(")
